d0b423641ac6b4f828764ed5f06f967bf76896bd
Koding <trenton.daniels@gmail.com>
2015-10-21 21:52:45 +0000

    finished for now

--- client/js/controllers/MainController.js
+++ client/js/controllers/MainController.js
@@ -2,7 +2,7 @@ app.controller('MainController', function($scope, $http, $window) {
     $scope.userName = null;
     $scope.userLogged = false;
     $scope.users = [];
-    
+    $scope.socketio = io.connect("http://utkk4a021056.trentondaniels.koding.io:8080");
 /* closeLogIn()
     Checks the REST client for existing user name,
     adds the user and hides the div if the name 
@@ -39,22 +39,34 @@ app.controller('MainController', function($scope, $http, $window) {
 */
      $scope.sendMessage = function() {
         //Will have to change URL to whatever is being used 
-        var socketio = io.connect("http://utkk4a021056.trentondaniels.koding.io:8080");
         var msg = ": " + document.getElementById("message_input").value;
         var user = $scope.userName;
-        socketio.emit("message_to_server", { message : msg, name : user});
+        $scope.socketio.emit("message_to_server", { message : msg, name : user});
         document.getElementById("message_input").value = "";
         };
         
-/* onbeforeunload()
-    If user leaves the page they are removed from the active
-    user list.
-    ******Currently it only works if the user refreshes the page*******
-*/
-    $window.unload = function() {
-        console.log($scope.userName);
-        if ($scope.userLogged) {
-            $http.get("deleteUser?q=" + $scope.userName);
+       
+       
+/* Client Side Socket
+    Recieves a message from the server and adds it to the chat stream
+*/ 
+    $scope.socketio.on("message_to_client", function(data) {
+        var name = document.getElementById("name").textContent;
+        document.getElementById("chatlog").innerHTML = (document.getElementById("chatlog").innerHTML) +
+        "<div class='row'><hr/>" + "<span class='chatBubble' id='" + data['idNum'] +"'>" + "<strong>" + 
+        data['name'] + "</strong>" + data['message'] + "</span></div>";
+        document.getElementById(data['idNum']).style.padding = '10px'
+        document.getElementById(data['idNum']).style.color = 'white'
+        document.getElementById(data['idNum']).style.borderRadius = '25px'
+        //Color codes the comments, blue if from that user, red otherwise
+        if (name === data['name']) {
+             document.getElementById(data['idNum']).style.backgroundColor = 'blue'
+             document.getElementById(data['idNum']).style.float = 'right';
         }
-    };
+        else {
+            document.getElementById(data['idNum']).style.backgroundColor = 'red';
+            document.getElementById(data['idNum']).style.float = 'left';
+        }
+        document.getElementById("chatlog").scrollTop = document.getElementById("chatlog").scrollHeight;
+    });
 });
